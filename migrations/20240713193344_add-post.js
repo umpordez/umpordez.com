f395519a270adb6f5bc0c545375b6d5764e45ff8
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-07-13 18:19:55 -0300

    add more posts

--- migrations/20240713193344_add-post.js
+++ migrations/20240713193344_add-post.js
@@ -17,12 +17,32 @@ export const up = async (knex) => {
         ) VALUES (
             'melhorando-a-legibilidade-de-codigos-com-design-patterns',
             'Melhorando a Legibilidade do Código com Design Patterns',
-            'Design Patterns',
+            'Design Patterns (ai)',
             'https://i3.ytimg.com/vi/CPNUQskQLlA/hqdefault.jpg',
             'Design Patterns são padrões de código que se repetem ao longo da nossa vida',
             'Deividy Metheler Zachetti',
             'https://avatars.githubusercontent.com/u/1174445?v=4',
             '2023-08-11'
+        ),
+        (
+            'polling-nodejs',
+            'Como atualizar sua UI em tempo real SEM websockets ',
+            'node.js & JavaScript (ai)',
+            'https://i3.ytimg.com/vi/tpMdzLOilUw/hqdefault.jpg',
+            'Polling é uma estratégia de atualização em tempo real em que um cliente envia repetidamente...',
+            'Deividy Metheler Zachetti',
+            'https://avatars.githubusercontent.com/u/1174445?v=4',
+            '2022-03-14'
+        ),
+        (
+            'github-actions-run-tests',
+            'A melhor forma de rodar testes automatícamente',
+            'node.js & tests & github (ai)',
+            'https://i3.ytimg.com/vi/GTAEWhfDbug/hqdefault.jpg',
+            'Primeiro, vamos remover tudo relacionado a Karma e Grunt do repositório. Em seguida, vamos instalar...',
+            'Deividy Metheler Zachetti',
+            'https://avatars.githubusercontent.com/u/1174445?v=4',
+            '2024-08-14'
         );
     `);
 };
@@ -33,7 +53,12 @@ export const up = async (knex) => {
  */
 export const down = async (knex) => {
     return knex.raw(`
-        DELETE FROM blog_posts where link = 'melhorando-a-legibilidade-de-codigos-com-design-patterns';
+        DELETE FROM blog_posts where link IN (
+            'melhorando-a-legibilidade-de-codigos-com-design-patterns',
+            'github-actions-run-tests',
+            'polling-nodejs'
+        );
+
 
     `);
 };
